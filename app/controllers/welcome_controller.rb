e6f24e9353bc234258902f5b0515b6a530bae489
Emmanuel Serna <eserna27@gmail.com>
2016-01-06 12:48:39 -0600

    now can view months spends

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    @spending = Spend.new(SpendingStore)
-    @spendings = Spend.current_month_spends(SpendingStore)
-    @month_total_spends = Spend.month_total_spends(@spendings)
   end
 end
